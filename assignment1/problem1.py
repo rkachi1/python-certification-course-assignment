7f066f55949460880ef100d08f2d7f78b27610bb
rkachi1 <rangarajkk@gmail.com>
2021-04-23 02:14:26 -0400

    added assignment 1 solution

---
+++ assignment1/problem1.py
@@ -0,0 +1,9 @@
+givenNum = int(input('Enter a given number'))
+i = 1
+while(i <= givenNum):
+    if(givenNum % i == 0):
+        if(i % 2 == 0):
+            print('Factor is even', i)
+        else:
+            print('factor is odd', i)
+    i = i + 1
