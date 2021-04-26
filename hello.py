4c987bec3a8d3b1a15772370fd9220d263ff3771
rkachi1 <rangarajkk@gmail.com>
2021-04-26 01:36:42 -0400

    changed to function

--- hello.py
+++ hello.py
@@ -1,1 +1,7 @@
-print('hello world')
+age = input('Enter a number\n')
+
+while(1):
+    if(age.isalpha or age.isalnum):
+        print('Please enter a valid input')
+    else:
+        print(age * 5)
